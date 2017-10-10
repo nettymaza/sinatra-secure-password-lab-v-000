b8cfb0b7c0bad677acb52e8f7d5d31673e83142e
Annette Maza <nettymaza@gmail.com>
2017-10-10 21:59:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
+  #your code here
   if params[:username] == "" || params[:password] == ""
       redirect '/failure'
     else
